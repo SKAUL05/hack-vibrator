4176e3ef810e75ca1d352b502db1abc61469ff08
SKAUL05 <kaul.sarath@gmail.com>
2024-08-03 11:06:39 +0530

    Home Screen Improvements

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,10 +5,12 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Image
-        source={require("../assets/bg.jpg")}
+        source={require("../assets/home_screen.jpg")}
         style={styles.backgroundImage}
       />
       <View style={styles.content}>
+        <Text style={styles.title}>iCAN</Text>
+        <Text style={styles.subtitle}>Banking made accessible</Text>
         <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.navigate("OTP Verification")}
@@ -29,14 +31,27 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   backgroundImage: {
-    width: 400,
-    height: 400,
+    width: 200,
+    height: 200,
     marginTop: -300,
-    resizeMode: "stretch",
+    marginLeft: 20,
   },
   content: {
     position: "absolute",
     bottom: 50,
+    alignItems: "center", // Center the text and button horizontally
+    marginBottom: 160,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#000", // Black or any color that contrasts with the background
+    marginBottom: 2, // Space between title and subtitle
+  },
+  subtitle: {
+    fontSize: 23,
+    color: "#333", // Darker color for subtitle
+    marginBottom: 50, // Space between subtitle and button
   },
   button: {
     paddingVertical: 12,
